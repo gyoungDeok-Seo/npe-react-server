de724c5cb2e2835dc42aad0d732ee08b233fe172
baek-si-hyun <qortlgus100@gmail.com>
2024-05-28 17:43:45 +0900

    240528 main update

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,50 @@
+import "./footer.css";
+function Footer() {
+  return (
+    <div className="container">
+      <div className="box">
+        <div className="footer-top">
+          <a target="_blank" href="https://publy.co/content/755">
+            <span className="terms">이용약관</span>
+          </a>
+          <div class="slate"></div>
+          <a target="_blank" href="https://publy.co/content/756">
+            <span class="privacy">개인정보 처리방침</span>
+          </a>
+        </div>
+        <div class="footer-mid">
+          <span>(주) 퍼블리</span>
+          <div class="tw-h-3 tw-w-px tw-bg-color-slate-300"></div>
+          <span>대표이사: 박소령</span>
+          <div class="tw-h-3 tw-w-px tw-bg-color-slate-300"></div>
+          <span>사업자 등록번호: 198-81-00096</span>
+          <div class="tw-h-3 tw-w-px tw-bg-color-slate-300"></div>
+          <span>서울시 강남구 테헤란로 503 하이브로빌딩 10층</span>
+          <div class="tw-h-3 tw-w-px tw-bg-color-slate-300"></div>
+          <span>통신 판매업: 2020-서울강남-02648</span>
+          <div class="tw-h-3 tw-w-px tw-bg-color-slate-300"></div>
+          <span>직업 정보: J1200020230004</span>
+          <div class="tw-h-3 tw-w-px tw-bg-color-slate-300"></div>
+          <span>유료직업소개사업: 제 2024-3220250-14-5-00011호</span>
+        </div>
+        <div class="tw-text-xs tw-text-color-slate-400  tw-text-center">
+          커리어리 서비스에서 제공하는 상품 정보에 대하여 (주)퍼블리는
+          통신판매중개자이며 통신판매의 당사자가 아닙니다.
+          <br />
+          따라서 (주)퍼블리는 상품, 거래 정보 및 가격에 대하여 어떠한 의무와
+          책임도 부담하지 않습니다.&nbsp;
+          <a
+            href="https://publy.co/content/7268"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <span class="tw-text-xs tw-text-color-slate-400 hover:tw-text-color-text-subtler tw-underline tw-underline-offset-2">
+              소비자분쟁해결기준
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Footer;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,29 @@
-import Header from "../components/header/Header"
-
-function Main(){
-  
-  return <Header></Header>
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+import MainContainer from "../container/main/MainContainer";
+import "./main.css";
+function Main() {
+  return (
+    <div>
+      <div
+        style={{
+          position: "fixed",
+          zIndex: 9999,
+          top: "16px",
+          left: "16px",
+          right: "16px",
+          bottom: "16px",
+          pointerEvents: "none",
+        }}
+      ></div>
+      <div>
+        <Header />
+        <div className="container">
+          <MainContainer />
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
-export default Main
+export default Main;
